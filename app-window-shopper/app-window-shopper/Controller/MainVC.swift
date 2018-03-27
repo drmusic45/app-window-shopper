d6489d42f28fe5fe08e3d3c0ccf82d8f067cb75e
mobilestudio <mobilestudio@MacBook-Pro-5.local>
2018-03-26 21:57:57 -0700

    logic programming

--- app-window-shopper/app-window-shopper/Controller/MainVC.swift
+++ app-window-shopper/app-window-shopper/Controller/MainVC.swift
@@ -13,8 +13,13 @@ class MainVC: UIViewController {
     @IBOutlet weak var hourlyWageTextField: CurrencyTextField!
     @IBOutlet weak var itemPriceTextField: CurrencyTextField!
     
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
         
         let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
         calcBtn.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
@@ -28,9 +33,22 @@ class MainVC: UIViewController {
     
     //The #selector part of calcBtn.addTarget is part of Objective-C and thus in Swift this function must have @objc before it
     @objc func calculate() {
-        
+        if let wageText = hourlyWageTextField.text, let priceText = itemPriceTextField.text {
+            if let wage = Double(wageText), let price = Double(priceText) {
+                view.endEditing(true)
+                resultLabel.isHidden = false
+                hoursLabel.isHidden = false
+                resultLabel.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
     }
 
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
+        hourlyWageTextField.text = ""
+        itemPriceTextField.text = ""
+    }
     
 
 
